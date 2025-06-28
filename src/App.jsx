dcf678c2243cff494718d482cdb1420540cd9de8
tester620 <dheerajverma82664442@gmail.com>
2025-06-28 11:54:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Home from './Home.jsx'
 import Header from './Header.jsx'
 import SpecificBlog from './SpecificBlog.jsx'
 import Footer from './Footer.jsx'
+import AllBlogs from './AllBlogs.jsx'
 
 const App = () => {
   return (
 
-      <div className="nunito-body">
+      <div className="nunito-body scroll-smooth">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blogs/:id" element={<SpecificBlog />} />
+          <Route path="/blogs" element={<AllBlogs />} />
+
         </Routes>
         <Footer/>
       </div>
